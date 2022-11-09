be35996f355844b408318d295f95ca06404cd38d
AndresMoreta20 <andresmoreta2020@gmail.com>
2022-11-08 20:19:52 -0500

    primer avance del qrscanner

--- src/pages/inicio.js
+++ src/pages/inicio.js
@@ -6,48 +6,54 @@ import useUser from "../hooks/useUser"
 import { useEffect, useState } from "react"
 import { getAllBadges } from "./api/badges"
 import { useRouter } from "next/router"
-import { auth,storage } from "../config/client"
+import { auth, storage } from "../config/client"
 import { getUser } from "./api/users/index"
 import CarouselComponent from "../components/carousel"
-import {getUserUnlockedBadges} from "./api/users/index"
+import { getUserUnlockedBadges } from "./api/users/index"
+import Scan from "./scan"
 
-export default function inicio (props){
+export default function inicio(props) {
   const router = useRouter()
   const user = useUser();
-  const [info,setInfo] = useState(null)
-  const [unlockedBadges,setUnlockedBadges] = useState([])
-  const [carrouselBadges,setCarrouselBadges] = useState(props.availableBadges)
+  const [info, setInfo] = useState(null)
+  const [unlockedBadges, setUnlockedBadges] = useState([])
+  const [carrouselBadges, setCarrouselBadges] = useState(props.availableBadges)
+  const [newBadgeUnlocked, setNewBadgeUnlocked] = useState(false)
+
+  function handleNewBadgeChange(screen){
+      setNewBadgeUnlocked(screen);
+  }
 
   useEffect(() => {
-    async function getUserInfo(){
+    async function getUserInfo() {
       setInfo(await getUser(user.uid))
     }
-    async function getBadges(){
+    async function getBadges() {
       setUnlockedBadges(await getUserUnlockedBadges(user.uid))
     }
-    if(user!== null && user!==undefined){
+    if (user !== null && user !== undefined) {
       getBadges()
       getUserInfo()
     }
   }, [user])
 
-  useEffect(()=>{
-    if(unlockedBadges.length>0){
+  useEffect(() => {
+    if (unlockedBadges.length > 0) {
       setCarrouselBadges(mapBadgesToCarrousel(unlockedBadges))
     }
-  },[unlockedBadges])
+  }, [unlockedBadges])
 
   //const [carrouselFinalBadges, setCarrouselFinalBadges] = useState(carrouselBadges);
-  const [carrouselFinalBadges, setCarrouselFinalBadges] = useState([{src:"/Insignia.png",title:"HOLA",unlocked:true},{src:"/Insignia.png",title:"ADIOS",unlocked:false}]);
+  const [carrouselFinalBadges, setCarrouselFinalBadges] = useState([{ src: "/Insignia.png", title: "HOLA", unlocked: true }, { src: "/Insignia.png", title: "ADIOS", unlocked: false }]);
 
-  function comparadorListas(){
+  function comparadorListas() {
     const badges = carrouselBadges;
-    for(let s = 0; s < badges.length; s++){
-      badges[s] = {...badges[i], unlocked:false}
+    for (let s = 0; s < badges.length; s++) {
+      badges[s] = { ...badges[i], unlocked: false }
     }
-    for(let i = 0; i < badges.length;i++){
-      for(let j = 0; j < unlockedBadges.length; j++){
-        if(carrouselBadges[i].title == unlockedBadges[j].title){
+    for (let i = 0; i < badges.length; i++) {
+      for (let j = 0; j < unlockedBadges.length; j++) {
+        if (carrouselBadges[i].title == unlockedBadges[j].title) {
           badges[i].unlocked = true
         }
       }
@@ -57,42 +63,53 @@ export default function inicio (props){
 
   return (<>
     <Barra logged={true} ></Barra>
-    <div className={style.insigniasUsuario}>
-      <div style={{textAlign:"left"}}>
-        <h2>Bienvenido {info?info.name:null}</h2>
-        <h2>Insignias recolectadas</h2>
-        <p>{unlockedBadges.length}</p>
-      </div>
-      <div className={[style.tamanioCarousel]}>
-        <CarouselComponent images={carrouselFinalBadges}/>
-      </div>
-      <button onClick={()=>{router.replace('/scan')}}>
-        <div className={style.botonQr}>
-        <Image src={qr} alt='qr_img' className={style.imagenBotonQR}/>
+    {newBadgeUnlocked == false &&
+      <div div className={style.insigniasUsuario}>
+        <div style={{ textAlign: "left" }}>
+          <h2>Bienvenido {info ? info.name : null}</h2>
+          <h2>Insignias recolectadas</h2>
+          <p>{unlockedBadges.length}</p>
         </div>
-        &nbsp;Agregar insignia
-      </button>
-    </div>
-    </>
+        <div className={[style.tamanioCarousel]}>
+          <CarouselComponent images={carrouselFinalBadges} />
+        </div>
+        <button onClick={() => { setNewBadgeUnlocked(true) }}>
+
+          <div className={style.botonQr}>
+            <Image src={qr} alt='qr_img' className={style.imagenBotonQR} />
+          </div>
+          &nbsp;Agregar insignia
+        </button>
+      </div>
+    }
+
+    {newBadgeUnlocked == true &&
+      <Scan onScanChange={setNewBadgeUnlocked}></Scan>
+    }
+
+  </>
   )
 }
 
-const mapBadgesToCarrousel=(badges)=>{
-  return(badges.map((badge)=>{
-    return{
-      src:badge.image,
-      alt:"",
-      title:badge.name,
+const mapBadgesToCarrousel = (badges) => {
+  return (badges.map((badge) => {
+    return {
+      src: badge.image,
+      alt: "",
+      title: badge.name,
     }
   }))
 }
 
-export const getServerSideProps=async()=>{
+export const getServerSideProps = async () => {
   const badges = await getAllBadges()
   const imgArray = mapBadgesToCarrousel(badges)
   return {
-    props:{
-      availableBadges:imgArray
+    props: {
+      availableBadges: imgArray
     }
   }
+
+
+
 }
--- src/pages/scan.js
+++ src/pages/scan.js
@@ -8,48 +8,80 @@ import Barra from "../components/Barra";
 import useUser from "../hooks/useUser";
 import { addUnlockedBadge } from "./api/badges";
 import { Button } from "@mui/material";
+import { async } from "@firebase/util";
+import { fontSize } from "@mui/system";
 
 function Scan(props) {
   const [data, setData] = useState("No hay insignia");
-  const [visible,setVisible] = useState(false)
+  const [visible, setVisible] = useState(false)
   const user = useUser()
   const router = useRouter()
 
-  useEffect(()=>{
-    if(user!== null && user!==undefined){
+  function changeData(texto) {
+    setData(texto);
+    props.onScanChange(false);
+    addUnlockedBadge(user.uid, { "name": data });
+  }
+
+  async function updateUser() {
+    addUnlockedBadge(user.uid, { "name": data })
+  }
+  useEffect(() => {
+    if (user !== null && user !== undefined) {
       setVisible(true)
     }
-  },[user])
+  }, [user])
 
   return (
     <>
-    
       <div className={styles.container}>
+        <h2>Escanea tu insignia</h2>
         {visible && <>
-          <QrReader 
+          <QrReader
             onResult={(result, error) => {
               if (!!result) {
+                // ()=>{changeData(result?.text)};
                 setData(result?.text);
+                // updateUser;
+                // props.onScanChange(false);
+
+                // ()=>{props.onScanChange(false); addUnlockedBadge(user.uid, { "name": data }) } 
               }
 
               if (!!error) {
                 console.info(error);
               }
 
-            } 
-          }
-  //this is facing mode : "environment " it will open backcamera of the smartphone and if not found will 
-  // open the front camera
-          constraints    ={{ facingMode:  "environment"  }}
+            }
+            }
+            //this is facing mode : "environment " it will open backcamera of the smartphone and if not found will 
+            // open the front camera
+            constraints={{ facingMode: "environment" }}
             style={{ width: "40%", height: "40%" }}
           />
-          <p>{data}</p>
-{/*           
+
+
+          {/*           
+          
             <Button onClick={()=>{addUnlockedBadge(user.uid,{"name":"FACEA"})}}  >FACEA</Button>
             <Button onClick={()=>{addUnlockedBadge(user.uid,{"name":"FICA"})}}  >FICA</Button> */}
-            {data!= "No hay insignia" && <Button onClick={()=>{addUnlockedBadge(user.uid,{"name":data})}}  >{data}</Button>}
-          </>}
-        <button onClick={()=>{router.push('/inicio')}}> Volver inicio</button>
+          {/* {data!= "No hay insignia" && <Button onClick={()=>{addUnlockedBadge(user.uid,{"name":data}), props.onScanChange(true)}}  >{data}</Button>} */}
+          {data != "No hay insignia" && <Button style={{
+            backgroundColor: "#8D2331",
+            border: "2px solid #8D2331",
+            borderRadius: "9vh",
+            height: "7vh",
+            margin: "10rem",
+            maxWidth: "30%",
+            textAlign: "center",
+            padding: "2rem",
+            color:"white",
+            fontSize: "3rem"
+
+          }} onClick={() => { props.onScanChange(false); addUnlockedBadge(user.uid, { "name": data }) }}>{data}</Button>}
+          {/* <input type={"text"} value={data} onChange={(e)=>{ props.onScanChange(false); addUnlockedBadge(user.uid, { "name": data }) } }readonly></input> */}
+        </>}
+        {/* <button onClick={() => { router.push('/inicio') }}> Volver inicio</button> */}
       </div>
     </>
   );
