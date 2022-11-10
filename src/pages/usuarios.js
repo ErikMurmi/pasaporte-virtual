6a17f0f8919aa767083697541fb32cb5c65d6f0b
Tito Jaramillo <titoj740@gmail.com>
2022-11-10 16:05:14 -0500

    Creación página para la lista de usuario

---
+++ src/pages/usuarios.js
@@ -0,0 +1,45 @@
+import { ClassNames } from "@emotion/react";
+import styles from "../styles/components.module.css"
+import Barra from "../components/Barra.js"
+
+const tablaUsuarios = [
+  { estudiante: "Tito Jaramilloo", normales: 8, bonus: 2, },
+  { estudiante: "Roberto Salazar", normales: 8, bonus: 2, },
+  { estudiante: "Fernando Jaramillo", normales: 8, bonus: 2, },
+  { estudiante: "Roberto Salazar", normales: 8, bonus: 2, },
+  { estudiante: "Tito Jaramilloo", normales: 8, bonus: 2, },
+  { estudiante: "Roberto Salazar", normales: 8, bonus: 2, },
+  { estudiante: "Tito Jaramilloo", normales: 8, bonus: 2, },
+  { estudiante: "Roberto Salazar", normales: 8, bonus: 2, },
+];
+
+export const usuarios = () => {
+  return (
+    <>
+      <div className={styles.usuarios}>
+        <h1>Lista de Usuarios</h1>
+        <button >Decargar lista (.xsl)</button>
+        <table>
+          <thead>
+            <tr>
+              <th>Estudiante</th>
+              <th>Insignias Normales</th>
+              <th>Insignias Bonus</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tablaUsuarios.map((usuariosMap) => (
+              <tr>
+                <th>{usuariosMap.estudiante}</th>
+                <th>{usuariosMap.normales}</th>
+                <th>{usuariosMap.bonus}</th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  )
+}
+
+export default usuarios;
