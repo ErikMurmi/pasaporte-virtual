e5bf1a65d38cb54e77297bf34cad49c66eef86d1
AndresMoreta20 <andresmoreta2020@gmail.com>
2022-11-14 23:41:19 -0500

    cambio nombre de archivos

--- src/pages/admin/usuarios.js
+++ src/pages/admin/usuarios.js
@@ -4,6 +4,21 @@ import Barra from "../../components/Barra.js"
 import { getAllUsers } from "../api/users";
 import { useState, useEffect } from "react";
 // import ReactHTMLTableToExcel from 'react-html-table-to-excel';
+// import { read, writeFileXLSX, XLSX } from "xlsx";
+import * as XLSX from 'xlsx/xlsx.mjs';
+
+/* load 'fs' for readFile and writeFile support */
+// import * as fs from 'fs';
+// XLSX.set_fs(fs);
+
+/* load 'stream' for stream support */
+import { Readable } from 'stream';
+XLSX.stream.set_readable(Readable);
+
+/* load the codepage support library for extended support with older formats  */
+import * as cpexcel from 'xlsx/dist/cpexcel.full.mjs';
+XLSX.set_cptable(cpexcel);
+
 
 const tablaUsuarios = [
   { estudiante: "Tito Jaramilloo", normales: 8, bonus: 2, id:1},
@@ -26,6 +41,7 @@ export const Usuarios = () => {
     type: "normal"
   }
   ]);
+  const [sheetData, setSheetData] = useState(null);
 
   useEffect(() => {
     loadUsers();
@@ -37,12 +53,21 @@ export const Usuarios = () => {
     setUserList(userL);
     // badgeList = badgeL;
   }
+
+  const exportTable =()=>{
+    var wb = XLSX.utils.book_new();
+    var ws = XLSX.utils.table_to_sheet(document.getElementById("tablaEstudiantes"));
+
+    XLSX.utils.book_append_sheet(wb, ws, "estudiantes1");
+
+    XLSX.writeFile(wb, "estudiantes.xlsx")
+  }
   return (
     <>
       <div className={styles.usuarios}>
         <h1>Lista de Usuarios</h1>
-        <button >Decargar lista (.xsl)</button>
-        <table>
+        <button onClick={exportTable}>Decargar lista (.xsl)</button>
+        <table id="tablaEstudiantes">
           <thead>
             <tr>
               <th>Estudiante</th>
