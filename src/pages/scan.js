b96645dcc9517760b0ddb65774cfb3de5a2e94b0
Tito Jaramillo <titoj740@gmail.com>
2022-11-15 00:28:24 -0500

    botones paginas video, scan

--- src/pages/scan.js
+++ src/pages/scan.js
@@ -11,6 +11,8 @@ import { Button } from "@mui/material";
 import { async } from "@firebase/util";
 import { fontSize } from "@mui/system";
 import { states } from "./inicio";
+import {AiFillHome} from "react-icons/ai"
+
 function Scan(props) {
   const [data, setData] = useState("No hay insignia");
   const [visible, setVisible] = useState(false)
@@ -85,6 +87,28 @@ function Scan(props) {
           {/* <input type={"text"} value={data} onChange={(e)=>{ props.onScanChange(false); addUnlockedBadge(user.uid, { "name": data }) } }readonly></input> */}
         </>}
         {/* <button onClick={() => { router.push('/inicio') }}> Volver inicio</button> */}
+        <Button href="/inicio" onClick={() => {props.onScanChange(states.waiting)}} 
+          style={{
+            width:"70%",
+            display: "flex",
+            backgroundColor: "#8D2331",
+            border: "2px solid #8D2331",
+            color: "white",
+            borderRadius: "10px",
+            alignItems: "center",
+            justifyContent: "center",
+            margin: "2vh auto 0",
+            fontSize: "2vh"
+        }}>
+
+          <div> 
+            <AiFillHome style={{
+                width: "3vh",
+                height: "3vh"
+            }}/>
+          </div>
+          &nbsp;Volver al inicio
+        </Button>
       </div>
     </>
   );
